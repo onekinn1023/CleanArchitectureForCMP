180a720dce0fe0583ac132aa5022444d58708f36
onekinn <qinwang1023@gmail.com>
2024-09-25 17:23:54 +0800

    clean code

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -2,16 +2,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
-import decompose.AppRootComponent
 import decompose.DecomposeMaterialApp
 import decompose.RootComponent
-import di.initKoin
-import org.koin.compose.getKoin
-import org.koin.compose.koinInject
-import org.koin.core.Koin
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
-import platform.Foundation.NSFileManager
 
 fun MainViewController() = ComposeUIViewController {
 //    App()
